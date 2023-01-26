6ff6642032c40e1fd78233e6cd5fef74eebee532
Ivan Verbitskyi <ivan.verbitskyj@gmail.com>
2023-01-26 22:26:43 +0200

    added: auth routes, user routes

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
-import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import cors from "cors";
+import express from "express";
 import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
+import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import { register } from "./controllers/auth.js";
 
-/* CONFIGURATIONS */
+// CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
@@ -23,7 +26,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-/* FILE STORAGE */
+// FILE STORAGE
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
 		cb(null, "public/assets");
@@ -34,9 +37,14 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-/* AUTORIZATION */
+// ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture"), register);
 
-/* MONGOOSE SETUP */
+// ROUTES
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+
+// MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
 mongoose.set("strictQuery", false);
 mongoose
